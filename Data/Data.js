411ba2391c26b00aff0ca104aa20a373ec6a548e
Asma triki <you@example.com>
2022-05-10 22:59:36 +0100

    mp

---
+++ Data/Data.js
@@ -0,0 +1,22 @@
+export const locations = [{
+    title: 'station 1', latitude: 32.55357374874022,
+    longitude: 9.687528423964977,
+    markerImage: "https://i.ibb.co/2PwKJWc/UA-Studios-2.png",
+    weight:3
+},
+
+{
+    title: 'station 2', latitude: 36.8002068,
+    longitude: 10.1857757,
+    markerImage: "https://i.ibb.co/2PwKJWc/UA-Studios-2.png",
+    weight:3
+},
+{
+    title: 'station 3', latitude: 34.163391564755216,
+    longitude: 9.295400362461805,
+    markerImage: "https://i.ibb.co/2PwKJWc/UA-Studios-2.png",
+    weight:3
+},
+
+
+]
